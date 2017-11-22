9a68cde9ef80e3a60e072d3466e2e644b36405e9
Ryan Howard <s3490922@student.rmit.edu.au>
2017-11-22 12:43:35 +1100

    Project created

---
+++ README.md
@@ -0,0 +1,3 @@
+# mm-chess
+
+A chess AI that runs on an implementation of the Minimax algorithm.
---
+++ src/main/java/game/Game.java
@@ -0,0 +1,13 @@
+package game;
+
+/**
+ * Hello world!
+ *
+ */
+public class Game
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
