86ae4007a890f6e2132f668e72e8c9fa4a9bdc58
Russell Long <russell._long@hotmail.com>
2017-12-22 23:51:45 +1100

    Added constructor and getter/setter to Move class

--- src/main/java/game/Move.java
+++ src/main/java/game/Move.java
@@ -3,6 +3,15 @@ package game;
 import java.awt.*;
 
 public class Move {
-    public Point startPosition;
-    public Point destinationPosition;
+    private Point startPosition;
+    private Point destinationPosition;
+
+    public Move(Point startPosition, Point destinationPosition){
+        this.startPosition = startPosition;
+        this.destinationPosition = destinationPosition;
+    }
+
+    public Point getStartPosition() { return this.startPosition; }
+    public Point getDestinationPosition() { return this.destinationPosition; }
+
 }
